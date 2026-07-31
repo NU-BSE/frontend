8540d32a7fe456ec02a7bfc20dc3495b1b1462fb
Айтбаев Даулет <55664062+Pureshko@users.noreply.github.com>
2026-07-31 13:34:21 +0500

    Add app/auth/verify.tsx

---
+++ app/auth/verify.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { EmailCodeScreen } from "@/auth/EmailCodeScreen";
+
+export default function VerifyEmailSignIn() {
+  return <EmailCodeScreen mode="login" />;
+}
